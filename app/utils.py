2bfbcfd3928e0ca1054894b56a2e061e3a192b3c
Arthur Meloyan <117680986+ArthurMeloyan@users.noreply.github.com>
2024-10-05 12:51:02 +0400

    added utils.py for hashing and verifing passwords

---
+++ app/utils.py
@@ -0,0 +1,40 @@
+import bcrypt
+from datetime import datetime, timedelta
+from typing import Optional
+from jose import jwt
+from pydantic import BaseModel
+
+SECRET_KEY = 'my_secret_key'
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 10
+
+def hash_password(password: str) -> str:
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed.decode('utf-8')
+
+
+def verify_password(hashed_password: str, plain_password: str) -> bool:
+    return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({'exp': expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    login: Optional[str] = None
+
+    
