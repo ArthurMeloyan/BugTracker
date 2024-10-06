1210e9919709026edfe3b42255de250b58742a12
ArthurMeloyan <meloyan.12@mail.ru>
2024-10-06 19:34:09 +0400

    little fix in utils.py

--- app/utils.py
+++ app/utils.py
@@ -11,12 +11,10 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 10
 
 
 def hash_password(password: str) -> str:
-    salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed.decode('utf-8')
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
 
-def verify_password(hashed_password: str, plain_password: str) -> bool:
+def verify_password(plain_password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
 
 
